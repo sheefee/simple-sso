b29ad530d73707a185e97fe73b0a736e599808c4
sheefee <573215750@qq.com>
2017-09-22 22:35:46 +0800

    动态传入表名字段名从数据库登录

--- simple-sso-server/src/main/java/com/sheefee/simple/sso/server/dao/UserDao.java
+++ simple-sso-server/src/main/java/com/sheefee/simple/sso/server/dao/UserDao.java
@@ -1,7 +1,10 @@
 package com.sheefee.simple.sso.server.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.sheefee.simple.sso.client.domain.User;
 
 public interface UserDao {
-	public User find(User user);
+	public User find(@Param("user") User user, @Param("table") String table, @Param("userid") String userid,
+			@Param("username") String username, @Param("password") String password);
 }
--- simple-sso-server/src/main/java/com/sheefee/simple/sso/server/service/UserServiceImpl.java
+++ simple-sso-server/src/main/java/com/sheefee/simple/sso/server/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sheefee.simple.sso.server.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sheefee.simple.sso.client.domain.User;
@@ -10,8 +11,16 @@ import com.sheefee.simple.sso.server.dao.UserDao;
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserDao userDao;
-	
+	@Value("${db.table}")
+	private String table;
+	@Value("${db.table.userid}")
+	private String userid;
+	@Value("${db.table.username}")
+	private String username;
+	@Value("${db.table.password}")
+	private String password;
+
 	public User find(User user) {
-		return userDao.find(user);
+		return userDao.find(user, table, userid, username, password);
 	}
 }
